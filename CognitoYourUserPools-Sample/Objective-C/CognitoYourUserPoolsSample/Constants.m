889feea249058e5e7ae26be27ae4a15f984c4e65
Jeremiah-Parrack <jeremiahlukus1@gmail.com>
2019-07-22 20:20:05 -0400

    Jeremiah | add obj C sample

---
+++ CognitoYourUserPools-Sample/Objective-C/CognitoYourUserPoolsSample/Constants.m
@@ -0,0 +1,23 @@
+//
+// Copyright 2014-2018 Amazon.com,
+// Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Amazon Software License (the "License").
+// You may not use this file except in compliance with the
+// License. A copy of the License is located at
+//
+//     http://aws.amazon.com/asl/
+//
+// or in the "license" file accompanying this file. This file is
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, express or implied. See the License
+// for the specific language governing permissions and
+// limitations under the License.
+//
+
+#import "Constants.h"
+
+AWSRegionType const CognitoIdentityUserPoolRegion = AWSRegionUSEast1;
+NSString *const CognitoIdentityUserPoolId = @"us-east-1_CRwwno5uJ";
+NSString *const CognitoIdentityUserPoolAppClientId = @"1tl5t6jjg1q0vjuh0q6itsebgu";
+NSString *const CognitoIdentityUserPoolAppClientSecret = @"m8om8mgvvi2k4cenpj3h8ji38llf3i8dkc225vonpdf4agt7q50!";
